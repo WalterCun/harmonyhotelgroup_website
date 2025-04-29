782ade37d3ffe949a9ae1b7882b23ce9587e710b
WalterCun <waltercunbustamante@gmail.com>
2025-04-29 13:09:49 -0500

    modify(config): update i18n settings and remove React integration
    Adjust i18n to include routing options and deactivate React setup.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 import {defineConfig} from 'astro/config';
-import react from '@astrojs/react';
+// import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    // i18n: {
-    //     locales: ["es", "en"],
-    //     defaultLocale: "es",
-    //
-    // },
+    i18n: {
+        locales: ['es', 'en'],
+        defaultLocale: 'es',
+        routing: {
+            prefixDefaultLocale: false,
+            // redirectToDefaultLocale: true,
+        }
+    },
     prefetch: true,
-    integrations: [tailwind(), react(), icon({
+    integrations: [tailwind(), icon({
             iconSets: [
                 {
                     name: 'lucide',
