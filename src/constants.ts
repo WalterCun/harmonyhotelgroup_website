30dcf5dfd7b57f7f53d34f0372cae576d70bd82a
WalterCun <waltercunbustamante@gmail.com>
2025-04-29 13:09:40 -0500

    add(constants): define app-wide constants for URLs and limits

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+// Limite de generadores
+export const maxAdults: number = 5
+export const maxChildren: number = 5
+
+// URL de Redes Sociales
+export const facebookUrl: string | null = 'https://www.facebook.com/harmonyhotel'
+export const twitterUrl: string | null = 'https://twitter.com/harmonyhotel'
+export const instagramUrl: string | null = 'https://www.instagram.com/harmonyhotel/'
+export const tiktokUrl: string | null = 'https://www.tiktok.com/@harmonyhotel'
+export const linkedinUrl: string | null = null
+export const youtubeUrl: string | null = null
+
+// URL de contacto
+export const whatsappUrl: string = 'https://wa.me/521555555555'
+export const emailUrl: string = 'mailto:<EMAIL>'
+export const phoneUrl: string = 'tel:+521555555555'
+export const websiteUrl: string = 'https://harmonyhotel.com'
+export const addressUrl: string = 'https://goo.gl/maps/1234567890'
+export const directionsUrl: string = 'https://goo.gl/maps/1234567890'
+
