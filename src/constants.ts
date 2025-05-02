05e07e2533eb8c5ddcb017e83fdaccdb49c33834
WalterCun <waltercunbustamante@gmail.com>
2025-05-01 19:07:37 -0500

    modify(constants): update limits and contact/social URLs

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,20 @@
 // Limite de generadores
-export const maxAdults: number = 5
-export const maxChildren: number = 5
+export const maxAdults: number = 10
+export const maxChildren: number = 9
 
 // URL de Redes Sociales
-export const facebookUrl: string | null = 'https://www.facebook.com/harmonyhotel'
-export const twitterUrl: string | null = 'https://twitter.com/harmonyhotel'
+export const facebookUrl: string | null = 'https://www.facebook.com/hotelmajestic2cuenca/'
+export const twitterUrl: string | null = null
 export const instagramUrl: string | null = 'https://www.instagram.com/harmonyhotel/'
 export const tiktokUrl: string | null = 'https://www.tiktok.com/@harmonyhotel'
 export const linkedinUrl: string | null = null
 export const youtubeUrl: string | null = null
+export const whatsappUrl: string | null = 'https://wa.me/593996146471'
 
 // URL de contacto
-export const whatsappUrl: string = 'https://wa.me/521555555555'
-export const emailUrl: string = 'mailto:<EMAIL>'
-export const phoneUrl: string = 'tel:+521555555555'
-export const websiteUrl: string = 'https://harmonyhotel.com'
+export const emailUrl: string = 'mailto:<gerencia@harmonyhotelgroup.com>'
+export const phoneUrl: string = 'tel:+593996146471'
+export const websiteUrl: string = 'https://harmonyhotelgroup.com'
 export const addressUrl: string = 'https://goo.gl/maps/1234567890'
 export const directionsUrl: string = 'https://goo.gl/maps/1234567890'
 
