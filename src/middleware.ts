7bf7ba45eb0043f8225d75e5e6d49135316229cb
WalterCun <waltercunbustamante@gmail.com>
2025-05-03 19:21:17 -0500

    add(middleware): initialize userMiddleware file

---
+++ src/middleware.ts
@@ -0,0 +1,4 @@
+// import {defineMiddleware, sequence} from 'astro:middleware';
+// import {middleware} from "astro:i18n";
+//
+// export const userMiddleware = defineMiddleware( async (ctx,next)=>)
