6ed052c55881f4cd9a914ba800852eee33720952
WalterCun <waltercunbustamante@gmail.com>
2025-05-30 17:25:20 -0500

    modify(ui) [skip ci]: refactor ReservationForm and remove Zod validation
    Streamline ReservationForm by removing Zod validation schema and unused code. Improve input handling and error display for better user experience.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,13 +2,23 @@ import {defineAction} from "astro:actions";
 import {z} from "astro:schema"
 
 export const server = {
-    reservation: defineAction({
+    validationForm: defineAction({
         accept: "form",
         input: z.object({
-            city: z.string().min(2, "City must be at least 2 characters")
+            city: z.string().min(2, "form.city.error"),
+            adults: z.string().min(1, "Por favor selecciona el número de adultos"),
+            children: z.string().optional(),
+            rooms: z.string().min(1, "Por favor selecciona el número de habitaciones"),
+
         }),
-        handler: async ({city}) => {
-            console.log("Action", city)
+        handler: async ({city, adults, rooms, children}) => {
+            console.log("Action recibido:", {city, adults, rooms});
+
+            return {
+                success: true,
+                message: 'Reserva procesada correctamente'
+            }
+
         }
     })
 }
